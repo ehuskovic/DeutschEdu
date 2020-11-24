3c2969fef2f13ff60a11bfa127154329c30dd0ee
ehuskovic <eh6169@g.rit.edu>
2020-11-24 20:00:56 +0100

    Initial Commit

---
+++ DeutschEdu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeutschEdu
+//
+//  Created by Ella Huskovic on 24/11/2020.
+//  Copyright © 2020 Ella Huskovic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
